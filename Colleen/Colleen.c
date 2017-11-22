00db429abc54433cc1e97c402acd17b2a1c66913
Mihai SEINIC <mseinic@student.42.fr>
2017-11-22 20:40:01 +0100

    final

--- Colleen/Colleen.c
+++ Colleen/Colleen.c
@@ -2,4 +2,8 @@
 /*
 M
 */
-int d(){return(0);}int main(){/*s*/char*s="#include <stdio.h>%c/*%cM%c*/%cint d(){return(0);}int main(){/*s*/char*s=%c%s%c;printf(s,10,10,10,10,34,s,34,10);return(d());}%c";printf(s,10,10,10,10,34,s,34,10);return(d());}
+int d(){return(0);}int main(){
+/*
+s
+*/
+char*s="#include <stdio.h>%c/*%cM%c*/%cint d(){return(0);}int main(){%c/*%cs%c*/%cchar*s=%c%s%c;printf(s,10,10,10,10,10,10,10,10,34,s,34,10);return(d());}%c";printf(s,10,10,10,10,10,10,10,10,34,s,34,10);return(d());}
--- Grace/Grace.c
+++ Grace/Grace.c
@@ -3,6 +3,6 @@
 m
 */
 #define T
-#define S "#include <stdio.h>%c/*%cm%c*/%c#define T%c#define S %c%s%c%c#define M int main(){fprintf(fopen(%cGrace_kid.c%c,%cw+%c),S,10,10,10,10,10,34,S,34,10,34,34,34,34,10,10);return(0);}%cM%c"
-#define M int main(){fprintf(fopen("Grace_kid.c","w+"),S,10,10,10,10,10,34,S,34,10,34,34,34,34,10,10);return(0);}
+#define S "#include <stdio.h>%c/*%cm%c*/%c#define T%c#define S %c%s%c%c#define M int main(){FILE*f=fopen(%cGrace_kid.c%c,%cw+%c);if(!f){return(1);}fprintf(f,S,10,10,10,10,10,34,S,34,10,34,34,34,34,10,10);fclose(f);return(0);}%cM%c"
+#define M int main(){FILE*f=fopen("Grace_kid.c","w+");if(!f){return(1);}fprintf(f,S,10,10,10,10,10,34,S,34,10,34,34,34,34,10,10);fclose(f);return(0);}
 M
